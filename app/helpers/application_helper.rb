c363c433c8bda518e3fd782fc3bff2f6f2b5863c
Ayron Sanfra <ayron.marinho@estudante.ufla.br>
2024-10-25 11:48:56 -0300

    feat: new models ant controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
-module ApplicationHelper
+# frozen_string_literal: true
+
+module ApplicationHelper # rubocop:disable Style/Documentation
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
