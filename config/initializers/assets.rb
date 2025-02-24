ac365ea07c0553e03198541d6dace72c5bded829
Ayron Sanfra <ayron.marinho@estudante.ufla.br>
2025-02-24 12:27:55 -0300

    fix: bootstrap working fine

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 Rails.application.config.assets.precompile << "bootstrap.min.js"
+Rails.application.config.assets.precompile += %w( application.css )
